2d39aacc6ee3f9becb6b4e5618f94448e83f4e66
mar1ab <92764905+mar1ab@users.noreply.github.com>
2022-04-15 16:35:22 -0700

    Created Weather component and corresponding css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  );
 }
 
 export default App;
