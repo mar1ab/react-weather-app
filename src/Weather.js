acada1e1e8d52af0b7f2a14dd9abc36917eee2d4
mar1ab <92764905+mar1ab@users.noreply.github.com>
2022-04-16 10:02:15 -0700

    Imported form portion from existing project

--- src/Weather.js
+++ src/Weather.js
@@ -2,5 +2,17 @@ import React from "react";
 import "./Weather.css";
 
 export default function Weather() {
-  return <div className="Weather"></div>;
+  return (
+    <div className="Weather">
+      <form id="search-form">
+        <input type="text" placeholder="Search city..." />
+        <button className="form-buttons">
+          <i className="fas fa-search"></i>
+        </button>
+        <button className="form-buttons">
+          <i className="fas fa-map-marker-alt"></i>
+        </button>
+      </form>
+    </div>
+  );
 }
