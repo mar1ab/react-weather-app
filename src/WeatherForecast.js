b24ac1351ab632ec41745d32a83428346d946f3f
mar1ab <92764905+mar1ab@users.noreply.github.com>
2022-04-22 17:59:22 -0700

    Moved existing html into WeatherForecast component

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,5 +1,39 @@
 import React from "react";
 
 export default function WeatherForecast() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="row forecast m-3" id="forecast">
+        <div className="col">
+          <strong>WED</strong>
+          <br />
+          15º/7º <br />
+          🌤
+        </div>
+        <div className="col">
+          <strong>THU</strong> <br />
+          14º/7º <br />
+          🌧
+        </div>
+        <div className="col">
+          <strong>FRI </strong>
+          <br />
+          16º/6º <br />
+          🌤
+        </div>
+        <div className="col">
+          <strong>SAT </strong>
+          <br />
+          13º/5º <br />
+          🌩
+        </div>
+        <div className="col">
+          <strong>SUN </strong>
+          <br />
+          15º/7º <br />
+          ☀️
+        </div>
+      </div>
+    </div>
+  );
 }
