1245a248bfbcc7b10738f557ea7e2abf73f01d0a
mar1ab <92764905+mar1ab@users.noreply.github.com>
2022-04-26 14:45:20 -0700

    Removed temperature conversion function

--- src/Temperature.js
+++ src/Temperature.js
@@ -1,84 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 
 export default function Temperature(props) {
-  const [unit, setUnit] = useState("celsius");
-
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  if (unit === "celsius") {
-    return (
-      <span className="Temperature">
-        {Math.round(props.currentCelsius)}
-        ºC
-        <br />
-        H:
-        {props.highCelsius}ºC | L:{props.lowCelsius}
-        ºC
-        <div className="btn-custom">
-          <div
-            className="btn-group"
-            role="group"
-            aria-label="Basic mixed styles example"
-          >
-            <button type="button" className="btn btn-primary">
-              ºC
-            </button>
-            <button
-              type="button"
-              className="btn btn-primary"
-              onClick={showFahrenheit}
-            >
-              ºF
-            </button>
-          </div>
-        </div>
-      </span>
-    );
-  } else {
-    let currentFahrentheit = Math.round((props.currentCelsius * 9) / 5 + 32);
-    let highFahrentheit = Math.round((props.highCelsius * 9) / 5 + 32);
-    let lowFahrentheit = Math.round((props.lowCelsius * 9) / 5 + 32);
-    return (
-      <span className="Temperature">
-        {currentFahrentheit}
-        ºF
-        <br />
-        H:
-        {highFahrentheit}
-        ºF | L:
-        {lowFahrentheit}
-        ºF
-        <div className="btn-custom">
-          <div
-            className="btn-group"
-            role="group"
-            aria-label="Basic mixed styles example"
-          >
-            <button
-              type="button"
-              className="btn btn-primary"
-              onClick={showCelsius}
-            >
-              ºC
-            </button>
-            <button
-              type="button"
-              className="btn btn-primary"
-              onClick={showFahrenheit}
-            >
-              ºF
-            </button>
-          </div>
-        </div>
-      </span>
-    );
-  }
+  return (
+    <span className="Temperature">
+      {Math.round(props.currentCelsius)}
+      ºC
+      <br />
+      H:
+      {props.highCelsius}ºC | L:{props.lowCelsius}
+      ºC
+    </span>
+  );
 }
